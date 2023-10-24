01779763c1401b9d32de796eb1e5d11a5779705c
Niklas Eicker <git@nikl.me>
2023-10-24 21:16:31 +0200

    Get the general workflow to work

--- src/app.rs
+++ src/app.rs
@@ -8,8 +8,6 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        // injects a stylesheet into the document <head>
-        // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
 
         // sets the document title
@@ -19,8 +17,34 @@ pub fn App() -> impl IntoView {
         <Router>
             <main>
                 <Routes>
-                    <Route path="" view=HomePage/>
-                    <Route path="/*any" view=NotFound/>
+                    <StaticRoute
+                        path="/"
+                        view=HomePage
+                        static_params=move || Box::pin(async move { StaticParamsMap::default() })
+                    />
+                    <StaticRoute
+                        path="/test/hallo"
+                        view=HomePage
+                        static_params=move || Box::pin(async move { StaticParamsMap::default() })
+                    />
+                    <StaticRoute
+                        path="404"
+                        view=NotFound
+                        static_params=move || Box::pin(async move { StaticParamsMap::default() })
+                    />
+                    <StaticRoute
+                        path="/*any"
+                        view=NotFound
+                        static_params=move || Box::pin(async move {
+                            let mut map = StaticParamsMap::default();
+                            map.insert(
+                                "any".to_string(),
+                                vec!["steve".to_string(), "vali".to_string()],
+                            );
+                            map
+                        })
+                    />
+
                 </Routes>
             </main>
         </Router>
@@ -43,21 +67,5 @@ fn HomePage() -> impl IntoView {
 /// 404 - Not Found
 #[component]
 fn NotFound() -> impl IntoView {
-    // set an HTTP status code 404
-    // this is feature gated because it can only be done during
-    // initial server-side rendering
-    // if you navigate to the 404 page subsequently, the status
-    // code will not be set because there is not a new HTTP request
-    // to the server
-    #[cfg(feature = "ssr")]
-    {
-        // this can be done inline because it's synchronous
-        // if it were async, we'd use a server function
-        let resp = expect_context::<leptos_actix::ResponseOptions>();
-        resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
-    }
-
-    view! {
-        <h1>"Not Found"</h1>
-    }
+    view! { <h1>"Not Found from Leptos"</h1> }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
+use leptos_actix::generate_route_list_with_ssg;
+use leptos_router::build_static_routes;
+
 #[cfg(feature = "ssr")]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     use actix_files::Files;
     use actix_web::*;
     use leptos::*;
-    use leptos_actix::{generate_route_list, LeptosRoutes};
+    use leptos_actix::LeptosRoutes;
     use test_leptos::app::*;
 
     let conf = get_configuration(None).await.unwrap();
-    let addr = conf.leptos_options.site_addr;
-    // Generate the list of routes in your Leptos App
-    let routes = generate_route_list(App);
+    let leptos_options = conf.leptos_options;
+    let addr = leptos_options.site_addr.clone();
+    let (routes, static_data_map) = generate_route_list_with_ssg(App);
+
+    build_static_routes(&leptos_options, App, &routes, &static_data_map)
+        .await
+        .unwrap();
     println!("listening on http://{}", &addr);
 
     HttpServer::new(move || {
-        let leptos_options = &conf.leptos_options;
         let site_root = &leptos_options.site_root;
 
         App::new()
