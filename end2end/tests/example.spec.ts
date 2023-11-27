c018f027ec8e3e2597faae322de50df63f4d25dc
Niklas Eicker <git@nikl.me>
2023-11-27 22:02:44 +0100

    Fix feature flag setup

--- end2end/tests/example.spec.ts
+++ end2end/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("homepage has title and links to intro page", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto("http://localhost:3000/steve");
 
   await expect(page).toHaveTitle("Welcome to Leptos");
 
--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,6 @@ async fn main() -> std::io::Result<()> {
             .service(favicon)
             .leptos_routes(leptos_options.to_owned(), routes.to_owned(), App)
             .app_data(web::Data::new(leptos_options.to_owned()))
-        //.wrap(middleware::Compress::default())
     })
     .bind(&addr)?
     .run()
