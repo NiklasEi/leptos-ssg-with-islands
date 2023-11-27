0d64601397a7796b66c40478e3be59d64fb9ace4
Niklas Eicker <git@nikl.me>
2023-11-27 22:15:59 +0100

    netlify configuration

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,18 @@ use leptos_router::build_static_routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use actix_files::Files;
-    use actix_web::*;
     use leptos::*;
-    use leptos_actix::LeptosRoutes;
     use test_leptos::app::*;
 
     let conf = get_configuration(None).await.unwrap();
     let leptos_options = conf.leptos_options;
-    let addr = leptos_options.site_addr.clone();
     let (routes, static_data_map) = generate_route_list_with_ssg(App);
 
     build_static_routes(&leptos_options, App, &routes, &static_data_map)
         .await
         .unwrap();
-    println!("listening on http://{}", &addr);
 
-    HttpServer::new(move || {
-        let site_root = &leptos_options.site_root;
-
-        App::new()
-            .route("/api/{tail:.*}", leptos_actix::handle_server_fns())
-            // serve JS/WASM/CSS from `pkg`
-            .service(Files::new("/pkg", format!("{site_root}/pkg")))
-            // serve other assets from the `assets` directory
-            .service(Files::new("/assets", site_root))
-            // serve the favicon from /favicon.ico
-            .service(favicon)
-            .leptos_routes(leptos_options.to_owned(), routes.to_owned(), App)
-            .app_data(web::Data::new(leptos_options.to_owned()))
-    })
-    .bind(&addr)?
-    .run()
-    .await
+    Ok(())
 }
 
 #[actix_web::get("favicon.ico")]
